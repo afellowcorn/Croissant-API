396348472b9c10f3eb58a834f7c812788347517e
Acorn <a.fellow.corn@gmail.com>
2024-11-29 20:09:04 +0100

    Icon and Exception
    - add croissant icon
    - failure to run main.py of mod will list exception output

--- croissant/mod.py
+++ croissant/mod.py
@@ -1,4 +1,5 @@
 import os
+import ujson
 
 class Mod:
 	def __init__(
@@ -7,6 +8,7 @@ class Mod:
         mod_info={}
     ):
 		self.folder = folder
+
 		self.mod_id = mod_info["mod_id"]
 		self.name = mod_info["name"]
 		self.version = mod_info["version"]
@@ -14,11 +16,18 @@ class Mod:
 		self.dependency_id = mod_info["dependency_id"]
 		self.dependency_name = mod_info["dependency_name"]
 
+		self.settings = {}
+
 	def call_ready(self):
 		for _, _, file in os.walk('mods'):
 			for file in file:
 				if file == "main.py":
 					try:
 						exec(open(f"mods/{self.folder}/{file}").read())
-					except:
-						print(f"\033[1m[Croissant]\033[0m ERROR: {self.folder} main.py cannot be executed")
+					except Exception as e:
+						print(f"\033[1m[Croissant]\033[0m ERROR: {self.folder} main.py cannot be executed: \033[31m{e}\033[0m")
+
+						return None
+				elif file == "settings.json":
+					with open(f"mods/{self.folder}/settings.json", 'r') as read_file:
+						self.settings = ujson.loads(read_file.read())
